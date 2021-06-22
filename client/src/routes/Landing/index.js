cfc47d6315e83d527e6823aec9939e543dd2e3fa
mariem bechikh ali <mariem.bechikhali@esprit.tn>
2021-06-22 22:03:24 +0100

    client 1.0

--- client/src/routes/Landing/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import signIn from "../../containers/SignIn";
-
-class Landing extends React.Component {
-  render() {
-    return <signIn />;
-  }
-}
-export default Landing;
